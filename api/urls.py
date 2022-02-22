ba77029fdb981d2f46ce0cbb15510c71cb024612
AmrEidAbdelrahman <amreid248@gmail.com>
2022-02-22 15:48:10 +0200

    add requirements.txt

--- api/urls.py
+++ api/urls.py
@@ -9,14 +9,16 @@ urlpatterns = [
 		path('appointments/upcoming/', AppointmentView.as_view({'get':'list'}), {"filter":"upcoming"}, name='appointment-upcoming'),
 		path('appointments/past/', AppointmentView.as_view({'get':'list'}), {"filter":"past"}, name='appointment-past'),
 		path('appointments/reserve/', AppointmentView.as_view({'post':'create'}), name='appointment-reserve'),
+		path('appointments/new/', AppointmentView.as_view({'get':'list'}), {"filter":"new"}, name='appointment-past'),
+
 		
 		# client and admin can cancel the appointment
-		path('appointments/<pk>/cancel/', AppointmentView.as_view({'get':'list', 'post':'create'}), name='appointment-cancel'),
+		path('appointments/<pk>/cancel/', AppointmentView.as_view({'put':'cancel'}), name='appointment-cancel'),
 		
 		# ONLY ADMIN
-		path('appointments/<pk>/approve/', AppointmentView.as_view({'get':'list', 'post':'create'}), name='appointment-approve'),
-		path('appointments/<pk>/mark-as-missed/', AppointmentView.as_view({'get':'list', 'post':'create'}), name='appointment-missed'),
-		path('appointments/<pk>/mark-as-finished/', AppointmentView.as_view({'get':'list', 'post':'create'}), name='appointment-finished'),
+		path('appointments/<pk>/approve/', AppointmentView.as_view({'put':'approve'}), name='appointment-approve'),
+		path('appointments/<pk>/mark-as-missed/', AppointmentView.as_view({'put':'missed'}), name='appointment-missed'),
+		path('appointments/<pk>/mark-as-finished/', AppointmentView.as_view({'put':'finished'}), name='appointment-finished'),
 
 		# reschedule appointment ( ONLY CLIENTS )
 		path('reschedule-request/', RescheduleRequestView.as_view({'get':'list', 'post':'create'}), name='reschedule-request'),
--- api/views.py
+++ api/views.py
@@ -25,8 +25,36 @@ class AppointmentView(ModelViewSet):
             return Appointment.objects.all().filter(date__gt=datetime.now()).order_by('date')
         elif filter == "past":
             return Appointment.objects.all().filter(date__lt=datetime.now()).order_by('date')
+        elif filter == "new":
+            return Appointment.objects.all().filter(approved=False, cancel=False, date__gt=datetime.now()).order_by('date')
         else:
             return Appointment.objects.all().order_by('date')
+
+    # for clients and admins
+    @action(detail=True, methods=['put'], name="appointment-cancel")
+    def cancel(self, request, pk=None):
+        print("####RR######")
+        req = Appointment.objects.filter(pk=pk).update(cancel=True)
+        return Response(status=200)
+
+
+    @action(detail=True, methods=['put'], name="appointment-cancel")
+    def approve(self, request, pk=None):
+        print("####aprove######")
+        req = Appointment.objects.filter(pk=pk).update(approved=True)
+        return Response(status=200)
+
+    @action(detail=True, methods=['put'], name="appointment-cancel")
+    def missed(self, request, pk=None):
+        print("####missed######")
+        req = Appointment.objects.filter(pk=pk).update(missed=True)
+        return Response(status=200)
+
+    @action(detail=True, methods=['put'], name="appointment-cancel")
+    def finished(self, request, pk=None):
+        print("####finished######")
+        req = Appointment.objects.filter(pk=pk).update(finished=True)
+        return Response(status=200)
             
     
 class RescheduleRequestView(ModelViewSet):
@@ -39,16 +67,16 @@ class RescheduleRequestView(ModelViewSet):
 
     @action(detail=True, methods=['put'], name="reschedule-approve")
     def approve(self, request, pk=None):
-    	req = RescheduleRequest.objects.filter(pk=pk).update(approved=True, refused=False)
-    	return Response(status=200)
+        req = RescheduleRequest.objects.filter(pk=pk).update(approved=True, refused=False)
+        return Response(status=200)
 
 
     @action(detail=True, methods=['put'], name='reschedule-cancel')
     def cancel(self, request, pk=None):
-    	req = RescheduleRequest.objects.filter(pk=pk).update(approved=False, refused=True)
-    	return Response(status=200)
+        req = RescheduleRequest.objects.filter(pk=pk).update(approved=False, refused=True)
+        return Response(status=200)
 
 
 class Home(ListView):
-	model = Appointment
-	
+    model = Appointment
+    
